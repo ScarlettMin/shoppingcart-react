cabd4c2286f95c6aa5bc58fe70beb2916117c7d7
Min Sooyoung <min.sooyoung@joinmedia.de>
2020-08-24 10:15:51 +0200

    Edit Cart.js

--- src/features/Cart.js
+++ src/features/Cart.js
@@ -5,7 +5,7 @@ import { fetchProducts } from './productSlice';
 import { addProductToCart, checkout, cartTotal } from './cartSlice';
 import { currency } from '../currency';
 
-class ShoppingCart extends Component {
+class Cart extends Component {
     constructor(props){
         super(props);
 
@@ -47,7 +47,7 @@ export default connect(
         cartItem : cart.items
     }), */
     { fetchProducts, addProductToCart, checkout } // 사용해서 연결할 actions 
-)(ShoppingCart);
+)(Cart);
 
 //카트 리스트 노출 ㅇ
 //카트 리스트에 카트 수량 노출 ㅇ
